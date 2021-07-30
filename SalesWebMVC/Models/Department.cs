5882c6616c8672a131ec2a5202e39977eee89b55
Alexandrade01 <solidwinter96@outlook.com>
2021-07-30 15:03:38 -0300

    Other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -9,5 +9,22 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller) { Sellers.Add(seller); }
+
+        //pegando cada vendendor da lista e somando todas as suas vendas em um periodo de data
+        public double TotalSales(DateTime initial, DateTime final) { return Sellers.Sum(sr => sr.TotalSales(initial, final)); }
+        
+            
+        
     }
 }
